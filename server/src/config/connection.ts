69ae3666f50b883227d90b14e27fbe6cc0e9ab8e
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-19 14:58:22 -0600

    trying different method for db connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,21 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 
-export default mongoose.connection;
+// export default mongoose.connection;
+
+const MONGODB_URI = process.env.MONGODB_URI || '';
+
+const db = async (): Promise<typeof mongoose.connection> => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Database connected.');
+    return mongoose.connection;
+  } catch (error) {
+    console.error('Database connection error:', error);
+    throw new Error('Database connection failed.');
+  }
+};
+
+export default db;
